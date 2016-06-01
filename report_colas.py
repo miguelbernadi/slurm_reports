5f560474aa3e22dfc134d82eb805a5bf8b3cd62d
Miguel Bernabeu <miguel.bernabeu@bsc.es>
2016-06-01 17:21:39 +0200

    Add docstrings to methods and classes

--- report_colas.py
+++ report_colas.py
@@ -27,6 +27,7 @@ time_bins = [0,60,120,300,600,1200,1800,3600,7200,10800,14400,18000,21600,25200,
 pattern_date_format = re.compile("^20[0-9][0-9]-[0-9]+-[0-9]+$")
 pattern_time = re.compile("[0-9]+")
 def parse_time(timestring):
+    """ Parse a string representing the duration of a job into a number of seconds elapsed. Supplied string is of format 00-00:00:00 """
     days = 0
     hours = 0
     minutes = 0
@@ -47,16 +48,19 @@ def parse_time(timestring):
     return days + hours + minutes + seconds
 
 def parse_date(datestring):
+    """ Convert a string representing a date to a naive datetime object """
     date_array = datestring.split("-")
     return datetime.date(int(date_array[0]),int(date_array[1]),int(date_array[2]))
 
 class UserRecord:
+    """ Structure to store aggregated job record statistics on a user basis """
     def __init__(self, name):
         self.username = name
         self.qos_jobs = {} # qos_name: [number, cpuh]
         self.partition_jobs = {}
 
     def add_record(self, cpu, cpu_hours, partition, qos):
+        """ Add a record's data to the user's history """
         if qos in self.qos_jobs:
             q = self.qos_jobs[qos]
             self.qos_jobs[qos] = [ q[0] + 1, q[1] + cpu_hours ]
@@ -69,12 +73,14 @@ class UserRecord:
             self.partition_jobs[partition] = [ 1, cpu_hours ]
 
     def total_jobs(self):
+        """ Return the number of jobs recorded for the user """
         count = 0
         for i in self.partition_jobs.keys():
             count += self.partition_jobs[i][0]
         return count
         
     def total_cpuh(self):
+        """ Return the number of compute hours recorded for the user """
         count = 0
         for i in self.partition_jobs.keys():
             count += self.partition_jobs[i][1]
@@ -87,6 +93,7 @@ class UserRecord:
         return self.partition_jobs[partition]
  
 class Report:
+    """ Analyses the data and displays formatted results """
     users = {} # hold user -> UserRecord relations
     times = [] # list of tuples (elapsed, timelimit, accuracy)
     total_entries = 0
@@ -105,6 +112,7 @@ class Report:
     pattern_cancelled_user = re.compile("^CANCELLED by [0-9]+$")
 
     def aggregate_job_data(self,job_fields):
+        """ Process the records one row at a time """
         if len(job_fields) < 9:
             pass
         self.total_entries += 1
@@ -123,17 +131,20 @@ class Report:
             self.times.append(tuple([duration, timelimit, accuracy]))
 
     def count_per_user(self, username, cpu, duration, partition, qos): 
+        """ Create per user recordings of job statistics """
         cpu_hours = self.compute_cpu_hours(cpu, duration)
         if username not in self.users:
             self.users[username] = UserRecord(username)
         self.users[username].add_record(cpu, cpu_hours, partition, qos) 
 
     def compute_cpu_hours(self, cpu, duration):
+        """ Compute the cpu hours of a job """
         total = parse_time(duration) * int(cpu) / 3600.
         self.total_compute_hours += total
         return total
 
     def count_job_status(self, job_status):
+        """ Compute the number of jobs in each status """
         if job_status == "COMPLETED":
             self.total_completed += 1
         elif job_status == "TIMEOUT":
@@ -156,6 +167,7 @@ class Report:
             self.total_unknown += 1
 
     def summary_report(self, title):
+        """ Main report output, including overall job execution details """
         print title
         print "Data gathered between %s - %s"    % (args.start, args.end)
         print "-" * 48
@@ -173,6 +185,7 @@ class Report:
             print "WARNING: unknown state: %s"   % self.total_unknown
 
     def user_consumption_report(self, total_avail_cpuh):
+        """ Report that shows per user consumption """
         print "%10s   %8s   %-12s" % ("Username", "Jobs", "Cpu_hours")
         print "-" * 42
         for key in sorted(self.users.keys()):
@@ -182,6 +195,7 @@ class Report:
         print "%10s   %8d   %9.2f (%6.2f %%)" % ("Total", self.total_entries, self.total_compute_hours, 100.0 * self.total_compute_hours / total_avail_cpuh)
 
     def timelimit_histogram(self):
+        """ Report showing a histogram table for time limits """
         values, limits = np.histogram([i[1] for i in self.times],bins=time_bins)
         print "Timelimit table"
         print "%15s | %6s | %8s - %8s" % ("time (s)", "amount", "percent", "cumulat")
@@ -193,6 +207,7 @@ class Report:
             print "%6d - %6d | %6d | %6.2f %% - %6.2f %%" % (limits[i], limits[i+1], values[i], percent, cum)
 
     def elapsed_histogram(self):
+        """ Report showing a histogram table for elapsed time """
         values, limits = np.histogram([i[0] for i in self.times],bins=time_bins)
         print "Elapsed table"
         print "%15s | %6s | %8s - %8s" % ("time (s)", "amount", "percent", "cumulat")
@@ -204,6 +219,7 @@ class Report:
             print "%6d - %6d | %6d | %6.2f %% - %6.2f %%" % (limits[i], limits[i+1], values[i], percent, cum)
 
     def accuracy_histogram(self):
+        """ Report showing a histogram table for job time accuracy """
         values, limits = np.histogram([i[2] for i in self.times],bins=[0,5,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000])
         print "Accuracy table"
         print "%13s | %6s | %8s - %8s" % ("accuracy (%)", "amount", "percent", "cumulat")
@@ -215,11 +231,13 @@ class Report:
             print "%5.0f - %5.0f | %6d | %6.2f %% - %6.2f %%" % (limits[i], limits[i+1], values[i], percent, cum)
     
 def dump_configuration(config):
+    """ Dump the configuration object's contents (debugging)"""
     for section in config.sections():
         for element in config.items(section):
             print section, element
 
 def valid_date_string(string):
+    """ Verify if the supplied string is a validly formatted date """
     if not pattern_date_format.match(string):
          msg="%r is not a valid date string. Format is YYYY-MM-DD" % string
          raise argparse.ArgumentTypeError(msg)
