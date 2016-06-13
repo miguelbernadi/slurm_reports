miguelbernadi/slurm_reports

commit ff3218a304a5f421c8f45167d93572b0d5b95726

Generalize histogram reports
Add a generic histogram report to replace the 3 specializations
previously used. In addition, fix the limits for the accuracy histogram
to useful values.

report_colas.py
@@ -192,41 +192,18 @@ class Report:
         print "-" * 55
         print     "%10s   %8d              %9.2f (%6.2f %%)" % ("Total", self.total_entries, self.total_compute_hours, 100.0 * self.total_compute_hours / total_avail_cpuh)
 
-    def timelimit_histogram(self):
-        """ Report showing a histogram table for time limits """
-        values, limits = np.histogram([i[1] for i in self.times],bins=time_bins)
-        print "Timelimit table"
-        print "%15s | %6s | %8s - %8s" % ("time (s)", "amount", "percent", "cumulat")
+    def histogram(self, title, header, bins):
+        """ Report showing a histogram table """
+        values, limits = np.histogram([i[2] for i in self.times],bins)
+        print title
+        print "%15s | %6s | %8s - %8s" % (header, "amount", "percent", "cumulat")
         print "-" * 46
         cum = 0
         for i in range(0, len(values)):
             percent = 100.0 * values[i] / self.total_completed
-            cum += percent
+            cum = 100.0  * sum(values[:i]) / self.total_completed
             print "%6d - %6d | %6d | %6.2f %% - %6.2f %%" % (limits[i], limits[i+1], values[i], percent, cum)
-
-    def elapsed_histogram(self):
-        """ Report showing a histogram table for elapsed time """
-        values, limits = np.histogram([i[0] for i in self.times],bins=time_bins)
-        print "Elapsed table"
-        print "%15s | %6s | %8s - %8s" % ("time (s)", "amount", "percent", "cumulat")
-        print "-" * 46
-        cum = 0
-        for i in range(0, len(values)):
-            percent = 100.0 * values[i] / self.total_completed
-            cum += percent
-            print "%6d - %6d | %6d | %6.2f %% - %6.2f %%" % (limits[i], limits[i+1], values[i], percent, cum)
-
-    def accuracy_histogram(self):
-        """ Report showing a histogram table for job time accuracy """
-        values, limits = np.histogram([i[2] for i in self.times],bins=[0,5,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000])
-        print "Accuracy table"
-        print "%13s | %6s | %8s - %8s" % ("accuracy (%)", "amount", "percent", "cumulat")
-        print "-" * 44
-        cum = 0
-        for i in range(0, len(values)):
-            percent = 100.0 * values[i] / self.total_completed
-            cum += percent
-            print "%5.0f - %5.0f | %6d | %6.2f %% - %6.2f %%" % (limits[i], limits[i+1], values[i], percent, cum)
+    
     
 def dump_configuration(config):
     """ Dump the configuration object's contents (debugging)"""
@@ -297,13 +274,13 @@ try:
     report.user_consumption_report(total_avail_cpuh)
     if args.histogram == 'elapsed' or args.histogram == 'all':
         print ""
-        report.elapsed_histogram()
+        report.histogram("Elapsed table", "time (s)", time_bins)
     if args.histogram == 'timelimit' or args.histogram == 'all':
         print ""
-        report.timelimit_histogram()
+        report.histogram("Timelimit table", "time (s)", time_bins)
     if args.histogram == 'accuracy' or args.histogram == 'all':
         print ""
-        report.accuracy_histogram()
+        report.histogram("Accuracy table", "accuracy (%)", [0,10,20,30,40,50,60,70,75,80,85,90,91,92,93,94,95,96,97,98,99,100,200])
 
 except subprocess.CalledProcessError as e:
     print "Execution error in:"

commit 633f3b39a5516468aa06deeae24cabc2bfeefed9

Fix histograms erroneous data ( issue #1 )
Commit ff3218a304a5f421c8f45167d93572b0d5b95726 introduced a bug that
provided the same data to all histograms, rendering all but one of them
useless. The fix has been to promote to the arglist the knowledge about
internal layout of Data.

This should be improved in future commits by using a method to create
the list of data values for the histogram (and offer those data points
to other functions as well). It would remove the need to know about
internal layout details and decouple Data from CLI code. Issue #2 will
track progress on this enhancement.

report_colas.py
@@ -160,9 +160,9 @@ class Report:
         print "-" * 55
         print     "%10s   %8d              %9.2f (%6.2f %%)" % ("Total", self.data.total_entries, self.data.total_compute_hours, 100.0 * self.data.total_compute_hours / total_avail_cpuh)
 
-    def histogram(self, title, header, bins):
+    def histogram(self, title, header, bins, data):
         """ Report showing a histogram table """
-        values, limits = np.histogram([i[2] for i in self.data.times],bins)
+        values, limits = np.histogram(data, bins)
         print title
         print "%15s | %6s | %8s - %8s" % (header, "amount", "percent", "cumulat")
         print "-" * 46
@@ -210,13 +210,13 @@ def args_histo(args):
     """ Present the appropriate histograms depending on CLI options """
     if args.mode == 'elapsed' or args.mode == 'all':
         print ""
-        report.histogram("Elapsed table", "time (s)", time_bins)
+        report.histogram("Elapsed table", "time (s)", time_bins, [i[0] for i in report.data.times])
     if args.mode == 'timelimit' or args.mode == 'all':
         print ""
-        report.histogram("Timelimit table", "time (s)", time_bins)
+        report.histogram("Timelimit table", "time (s)", time_bins, [i[1] for i in report.data.times])
     if args.mode == 'accuracy' or args.mode == 'all':
         print ""
-        report.histogram("Accuracy table", "accuracy (%)", [0,10,20,30,40,50,60,70,75,80,85,90,91,92,93,94,95,96,97,98,99,100,200])
+        report.histogram("Accuracy table", "accuracy (%)", [0,10,20,30,40,50,60,70,75,80,85,90,91,92,93,94,95,96,97,98,99,100,200], [i[2] for i in report.data.times])
 
 # Main
 sacct_command = [